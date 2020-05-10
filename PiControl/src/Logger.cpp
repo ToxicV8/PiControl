95e695a19629c86230d23da157f6a89dd87b9b66
Julian Kunzelmann <lasertrap00@gmail.com>
2020-05-10 18:49:10 +0200

    Update Logger.cpp

--- PiControl/src/Logger.cpp
+++ PiControl/src/Logger.cpp
@@ -11,17 +11,17 @@ void Logger::Log(const char* format, ...)
     va_list args;
     va_start (args, format);
     std::string final;
-	int length;
-	va_list apStrLen;
-	va_copy(apStrLen, args);
-	length = vsnprintf(NULL, 0, format, apStrLen);
-	va_end(apStrLen);
-	if (length > 0) {
-		final.resize(length);
-		vsnprintf((char *)final.data(), final.size() + 1, format, args);
-	}
+    int length;
+    va_list apStrLen;
+    va_copy(apStrLen, args);
+    length = vsnprintf(NULL, 0, format, apStrLen);
+    va_end(apStrLen);
+    if (length > 0) {
+        final.resize(length);
+        vsnprintf((char *)final.data(), final.size() + 1, format, args);
+    }
 
-	g_pTelegramManager->SendToAllChats(final);
+    g_pTelegramManager->SendToAllChats(final);
     printf ((final + "\n").c_str());
     va_end (args);
 }
