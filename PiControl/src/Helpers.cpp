78c09b037b180469c4584d6b277323080b67675e
Julian Kunzelmann <lasertrap00@gmail.com>
2020-05-10 18:47:22 +0200

    Update Helpers.cpp

--- PiControl/src/Helpers.cpp
+++ PiControl/src/Helpers.cpp
@@ -19,13 +19,13 @@ bool Helpers::stringToBool(std::string str)
 void Helpers::split(const std::string& s, char delim, std::vector<std::string>& elems)
 {
 
-	std::stringstream ss;
-	ss.str(s);
-	std::string item;
-	while (std::getline(ss, item, delim))
-	{
-		elems.push_back(item);
-	}
+    std::stringstream ss;
+    ss.str(s);
+    std::string item;
+    while (std::getline(ss, item, delim))
+    {
+        elems.push_back(item);
+    }
 }
 
 Helpers::~Helpers()
